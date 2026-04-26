628f4b8d389615018203c79bbd5495462b1f579f
yuri <yurifigueiramenezes@gmail.com>
2026-04-26 15:33:56 -0300

    ajusta porta do apache para render

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+PORT="${PORT:-80}"
+
+sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
+sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
