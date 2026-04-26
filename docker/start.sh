6b8458494c8dff96f6e009faebfe0ef9e57072e4
yuri <yurifigueiramenezes@gmail.com>
2026-04-26 15:22:49 -0300

    corrige script de inicializacao do render

---
+++ docker/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+apache2-foreground
