d504a78545236cc46d3bb4efab8318e34f8d0d70
yuri <yurifigueiramenezes@gmail.com>
2026-04-26 15:52:07 -0300

    limpa variaveis de ambiente no render

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,14 @@ set -e
 
 PORT="${PORT:-8000}"
 
+if [ -n "${APP_URL:-}" ]; then
+    export APP_URL="$(printf '%s' "$APP_URL" | tr -d '\r\n\t')"
+fi
+
+if [ -n "${APP_KEY:-}" ]; then
+    export APP_KEY="$(printf '%s' "$APP_KEY" | tr -d '\r\n\t')"
+fi
+
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
